5b9d823a1f75152e60743e0428bc5cc9c378ddc0
Gaël PORT <gael.port@rbs.fr>
2011-09-28 14:36:13 +0200

    [FIX #43864] Appel à une classe de compatibilityos

--- lib/services/VideoService.class.php
+++ lib/services/VideoService.class.php
@@ -108,7 +108,7 @@ class videos_VideoService extends f_persistentdocument_DocumentService
 			'background-image' => 'url(' . MediaHelper::getIcon('video', 'small') . ')'
 		);
 		$style = f_util_HtmlUtils::buildStyleAttribute($styleAttributes);
-		$nodeAttributes[f_tree_parser_AttributesBuilder::HTMLLINK_ATTRIBUTE] = '<a rel="cmpref:' . $document->getId() . '" title="' . $title . '" href="#" lang="' . $lang . '" class="document-dummy" style="' . $style . '">' . $title . '</a>';
+		$nodeAttributes['htmllink'] = '<a rel="cmpref:' . $document->getId() . '" title="' . $title . '" href="#" lang="' . $lang . '" class="document-dummy" style="' . $style . '">' . $title . '</a>';
 		
 	}
 	
