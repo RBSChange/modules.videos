ad4cde085167141d8edb4cdbde43833665ef654f
Gaël PORT <gael.port@rbs.fr>
2012-08-23 14:31:08 +0200

    [FIX #65132] Player copié uniquement lors de l'installation du module

--- setup/initData.php
+++ setup/initData.php
@@ -1,45 +1,11 @@
 <?php
 /**
- * videos_Setup
- * @package modules.video
+ * @package modules.videos
  */
 class videos_Setup extends object_InitDataSetup
 {
-	/**
-	 * This methode is calls when using the builder.
-	 * change import-init-data
-	 *
-	 * look methode class
-	 * @return void
-	 */
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-       	 	$scriptReader->executeModuleScript('videos', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
-		
-		try
-		{
-			$this->importPlayer();
-		}
-		catch (Exception $e)
-		{
-			echo $e->getMessage()."\n";
-		}
-	}
-
-	private function importPlayer()
-	{
-		$playerPath = FileResolver::getInstance()->setPackageName('modules_videos')
-			->setDirectory('player')->getPath('jwplayer.swf');
-		$destPath = f_util_FileUtils::buildWebeditPath('media', 'frontoffice', 'jwplayer.swf');
-		f_util_FileUtils::cp($playerPath, $destPath, f_util_FileUtils::OVERRIDE);
+		$this->executeModuleScript('init.xml');
 	}
 }
