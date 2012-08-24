765e1e4b4d7fc45cfa3187ccae52d2b2316404de
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-08-24 09:59:35 +0200

    [FIX] Add missing function createQuery() on videos_PreferencesService

--- lib/services/PreferencesService.class.php
+++ lib/services/PreferencesService.class.php
@@ -21,6 +21,15 @@ class videos_PreferencesService extends f_persistentdocument_DocumentService
 	}
 
 	/**
+	 * Create a query based on 'modules_videos/playlist' model
+	 * @return f_persistentdocument_criteria_Query
+	 */
+	public function createQuery()
+	{
+		return $this->getPersistentProvider()->createQuery('modules_videos/preferences');
+	}
+	
+	/**
 	 * @return videos_persistentdocument_preferences
 	 */
 	public function getNewDocumentInstance()
