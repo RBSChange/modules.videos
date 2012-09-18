e2559fa744c389cc2b10e9f2e19f279789692982
Gaël PORT <gael.port@rbs.fr>
2012-09-18 09:32:17 +0200

    [NEW] Merge with 3.6

--- setup/initData.php
+++ setup/initData.php
@@ -1,44 +1,11 @@
 <?php
 /**
- * videos_Setup
  * @package modules.video
  */
 class videos_Setup extends object_InitDataSetup
 {
-	/**
-	 * This methode is calls when using the builder.
-	 * change import-init-data
-	 *
-	 * look methode class
-	 * @return void
-	 */
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-	   	 	$scriptReader->executeModuleScript('videos', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
-		
-		try
-		{
-			$this->importPlayer();
-		}
-		catch (Exception $e)
-		{
-			echo $e->getMessage()."\n";
-		}
-	}
-
-	private function importPlayer()
-	{
-		$playerPath = change_FileResolver::getNewInstance()->getPath('modules', 'videos', 'player', 'jwplayer.swf');
-		$destPath = f_util_FileUtils::buildProjectPath('media', 'frontoffice', 'jwplayer.swf');
-		f_util_FileUtils::cp($playerPath, $destPath, f_util_FileUtils::OVERRIDE);
+		$this->executeModuleScript('init.xml');
 	}
 }
